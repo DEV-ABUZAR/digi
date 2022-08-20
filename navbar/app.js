d21dd5c89d1744f8170fc3be8a955ccf6d7d4abc
Dev-Abuzar <mianzari07@gmail.com>
2022-08-20 17:00:35 +0500

    hello

---
+++ navbar/app.js
@@ -0,0 +1,10 @@
+var userName = "abuzar";
+console.log(userName);
+userName = "younas";
+console.log(userName);
+var firstNum = 2;
+var secondNumber = 30;
+var thirdNumber = firstNum * secondNumber;
+console.log(thirdNumber);
+var num = 78;
+console.log(num);
---
+++ navbar/app.ts
@@ -0,0 +1,11 @@
+var userName:string="abuzar"
+console.log(userName);
+userName="younas"
+console.log(userName);
+var firstNum:number=2
+var secondNumber:number=30
+var thirdNumber:number=firstNum*secondNumber
+console.log(thirdNumber);
+
+var num:number=78
+console.log(num);
